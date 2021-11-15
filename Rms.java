13815e3700d2bea084b868575dfd228b818fd65e
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2021-11-15 20:00:14 +0200

    Update Rms.java

--- Rms.java
+++ Rms.java
@@ -1,31 +1,50 @@
 package rms;
+
 import java.util.*;
-public class Rms 
-{
-    public static void main(String[] args) 
-    {
+
+public class Rms {
+
+    public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
+        branches test = new branches();
+        menu test2 = new menu();
         login s1 = new login();
         owner s2 = new owner();
         s1.createfile();
-
-        System.out.print("** Welcome to Restaurant Management System! **\n"
-                + "** Please input a number for access type. (1 = owner, 2 = customer, 3 = waiter)\n"
-                + "* Access Type: ");
+        System.out.print(" Welcome to the system!\n"
+                + "** Please input a number for access type. (1 = Owner, 2 = Customer, 3 = Waiter)\n"
+                + " ACCESS TYPE: ");
         int access_type = cin.nextInt();
         cin.nextLine();
-        System.out.print("** Enter your login details.\n"
-                + "* Username: ");
-        String username = cin.nextLine();
-        System.out.print("* Password: ");
-        String password = cin.nextLine();
+        if (access_type == 1) {
+            System.out.print("** Enter your login details.\n"
+                    + "* Username: ");
+            String username = cin.nextLine();
+            System.out.print("* Password: ");
+            String password = cin.nextLine();
 
-        s1.writefile(username, password);
-        s2.readfile();
+            s1.writefile(username, password);
+            s2.readfile();
+            System.out.println("Enter 1 to delete the files or anthing else to leave it:");
+            int choose = cin.nextInt();
+            s2.deletefile(choose);
+        } else if (access_type == 2) {
+            test.chooseBranch();
+            test2.orderFood();
+        }
+        if (access_type == 3) {
+            System.out.print("** Enter your login details.\n"
+                    + "* Username: ");
+            String username = cin.nextLine();
+            System.out.print("* Password: ");
+            String password = cin.nextLine();
 
-        System.out.println("Enter 1 to delete the files or anthing else to leave it:");
-        int choose = cin.nextInt();
-        s2.deletefile(choose);
+            s1.writefile(username, password);
+            s2.readfile();
+            System.out.println("Enter 1 to delete the files or anthing else to leave it:");
+            int choose = cin.nextInt();
+            s2.deletefile(choose);
+        }
 
     }
 }
