ba4d4a4743c7a02ad783fc1bb11ac5da3c94966e
seif <74967136+seif1118@users.noreply.github.com>
2021-11-15 20:11:37 +0200

    Update Rms.java

--- Rms.java
+++ Rms.java
@@ -1,50 +1,50 @@
 package rms;
 
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Rms {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner cin = new Scanner(System.in);
-        branches test = new branches();
-        menu test2 = new menu();
         login s1 = new login();
         owner s2 = new owner();
+        branches test = new branches();
+        menu test2 = new menu();
         s1.createfile();
-        System.out.print(" Welcome to the system!\n"
-                + "** Please input a number for access type. (1 = Owner, 2 = Customer, 3 = Waiter)\n"
-                + " ACCESS TYPE: ");
+
+        System.out.print("** Welcome to Restaurant Management System! **\n"
+                + "** Please input a number for access type. (1 = owner, 2 = customer, 3 = waiter)\n"
+                + "* Access Type: ");
         int access_type = cin.nextInt();
-        cin.nextLine();
         if (access_type == 1) {
-            System.out.print("** Enter your login details.\n"
-                    + "* Username: ");
-            String username = cin.nextLine();
-            System.out.print("* Password: ");
-            String password = cin.nextLine();
+            System.out.println("press 1 to enter new username and password");
+            System.out.println("press 2 to delete all the file");
+            System.out.println("press 3 to delete one account");
+            int pick = cin.nextInt();
+            cin.nextLine();
+            if (pick == 1) {
+                System.out.print("** Enter your login details.\n"
+                        + "* Username: ");
+                String username = cin.nextLine();
+                System.out.print("* Password: ");
+                String password = cin.nextLine();
 
-            s1.writefile(username, password);
-            s2.readfile();
-            System.out.println("Enter 1 to delete the files or anthing else to leave it:");
-            int choose = cin.nextInt();
-            s2.deletefile(choose);
+                s1.writefile(username, password);
+                s2.readfile();
+            } else if (pick == 2) {
+                test.chooseBranch();
+                test2.orderFood();
+            } else if (pick == 3) {
+                owner fileSearch = new owner();
+                System.out.println("enter the username to be deleted:");
+                String userdelete = cin.nextLine();
+                fileSearch.parseFile("D:\\study\\work\\oop\\project.txt", userdelete);
+            }
         } else if (access_type == 2) {
-            test.chooseBranch();
-            test2.orderFood();
-        }
-        if (access_type == 3) {
-            System.out.print("** Enter your login details.\n"
-                    + "* Username: ");
-            String username = cin.nextLine();
-            System.out.print("* Password: ");
-            String password = cin.nextLine();
 
-            s1.writefile(username, password);
-            s2.readfile();
-            System.out.println("Enter 1 to delete the files or anthing else to leave it:");
-            int choose = cin.nextInt();
-            s2.deletefile(choose);
         }
-
     }
+
 }
+
