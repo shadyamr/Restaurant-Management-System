7675b9e801372275b1fb08f7b2ea7cb7afb9195f
seif <74967136+seif1118@users.noreply.github.com>
2021-11-15 20:13:27 +0200

    Update owner.java

--- owner.java
+++ owner.java
@@ -1,20 +1,32 @@
 package rms;
 
-import java.io.*;
+import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.NumberFormatException;
 import static java.lang.System.exit;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
 
 public class owner {
 
     public void readfile() {
         try {
-            File myobj = new File("C:\\Users\\PC\\Desktop\\project.txt");
+            String strcurrentline;
+            //File myobj = new File("D:\\study\\work\\oop\\project.txt");
+            BufferedReader myobj = new BufferedReader(new FileReader("D:\\study\\work\\oop\\project.txt"));
             try (Scanner myreader = new Scanner(myobj)) {
-                while (myreader.hasNextLine()) {
-                    String data = myreader.nextLine();
-                    System.out.println(data);
+                while ((strcurrentline = myobj.readLine()) != null) {
+
+                    System.out.println(strcurrentline);
                 }
+                int data = myobj.read();
+                while (data != -1) {
+                    data = myobj.read();
+                }
+                myobj.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -22,8 +34,18 @@ public class owner {
         }
     }
 
+    public void parseFile(String fileName, String searchStr) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(fileName));
+        while (scan.hasNext()) {
+            String line = scan.nextLine().toLowerCase();
+            if (line.contains(searchStr)) {
+                System.out.println(line);
+            }
+        }
+    }
+
     public void deletefile(int choose) {
-        File myobj = new File("C:\\Users\\PC\\Desktop\\project.txt");
+        File myobj = new File("D:\\study\\work\\oop\\project.txt");
         if (choose == 1) {//handle the error if the user enters letter not a number
             if (myobj.delete()) {
                 System.out.println(myobj.getName() + ":file is deleted successfully");
@@ -36,8 +58,10 @@ public class owner {
     }
 
     public String delete(String username, String password) {
+        File myobj = new File("D:\\study\\work\\oop\\project.txt");
         username = "";
         password = "";
         return null;
     }
+
 }
