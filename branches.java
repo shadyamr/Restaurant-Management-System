027b830a53b43fd9a8c9c31ab9452bd9b861b0e5
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2021-11-15 23:52:07 +0200

    Update branches.java

--- branches.java
+++ branches.java
@@ -1,11 +1,19 @@
 package rms;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.*;
+
 public class branches {
+
     Scanner scan = new Scanner(System.in);
-    protected String[] branchName = new String[]{"El-Nozha", "El-Maady", "Masr El-Gdeda"};
     protected int choosenBranchName;
     protected String choosenBranch;
     protected boolean check = true;
+    protected ArrayList<String> branchesNames = new ArrayList<>(Arrays.asList("El-Nozha", "El-Maady", "Masr El-Gdeda"));
+
     public branches() {
         this.choosenBranchName = 0;
     }
@@ -14,37 +22,41 @@ public class branches {
         this.choosenBranchName = 0;
         this.choosenBranch = null;
     }
-    
+
     public void chooseBranch() {
-        System.out.println("Welcome to our restaurant!\n"
-                + "Please choose one of our branches on the screen. \n"
-                + "1 for our " + branchName[0] + " branch.\n"
-                + "2 for our " + branchName[1] + " branch.\n"
-                + "3 for our " + branchName[2] + " branch.\n");
-        
+        System.out.println("Please choose one of our branches on the screen.\n");
+        for (int i = 0; i < branchesNames.size(); i++) {
+            System.out.println(i + " for our " + branchesNames.get(i) + " branch.\n");
+        }
         do {
             check = true;
             choosenBranchName = scan.nextInt();
-            if (choosenBranchName > 3) {
+            if (choosenBranchName > branchesNames.size()) {
                 check = false;
                 System.out.println("Invalid option, please enter a valid one!");
             }
-        switch (choosenBranchName) {
-            case 1:
-                System.out.println("Thanks for choosing the " + branchName[0] + " branch of ours.\n");
-                choosenBranch = branchName[0];
-                break;
-            case 2:
-                System.out.println("Thanks for choosing the " + branchName[1] + " branch of ours.\n");
-                choosenBranch = branchName[1];
-                break;
-            case 3:
-                System.out.println("Thanks for choosing the " + branchName[2] + " branch of ours.\n");
-                choosenBranch = branchName[2];
-                break;
-            default:
-                break;
+        } while (!check);
+    }
+
+    public void addBranch() {
+        System.out.println("Enter the name of the new branch you want to add.\n");
+        String x;
+        x = scan.nextLine();
+        branchesNames.add(x);
+    }
+
+    public void writefile() {
+        try {
+            try (FileWriter mywrite = new FileWriter("D:\\MIU Courses\\Object Oriented Programming\\branches.txt", true)) {
+                {                 for (int i = 0; i < branchesNames.size(); i++) {
+            mywrite.write(branchesNames.get(i)+ "\n");
+        }                    
+                }
+                mywrite.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("An error occurred.");
+            ex.printStackTrace();
         }
-    }while (!check);
     }
 }
