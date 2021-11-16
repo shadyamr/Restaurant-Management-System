34a861b1861d210ccdffdcd46a67a99f7d075d2a
seif <74967136+seif1118@users.noreply.github.com>
2021-11-16 16:48:08 +0200

    Update Rms.java

--- Rms.java
+++ Rms.java
@@ -18,28 +18,48 @@ public class Rms {
                 + "* Access Type: ");
         int access_type = cin.nextInt();
         cin.nextLine();
-        System.out.print("Enter your username and password.\n"
-                + "Enter your login details.\n"
-                + "* Username: ");
-        String username = cin.nextLine();
-        System.out.print("* Password: ");
-        String password = cin.nextLine();
-        s1.writefile(username, password);
         if (access_type == 1) {
-            System.out.println("Press 1 to edit the restaurant's system.");
-            System.out.println("Press 2 to delete an account.");
-            int pick = cin.nextInt();
-            if (pick == 1) {
-            test3.chooseOption();
-            } else if (pick == 2) {
-                owner fileSearch = new owner();
-                System.out.println("Enter the username to be deleted:");
-                String userdelete = cin.nextLine();
-                fileSearch.parseFile("D:\\MIU Courses\\Object Oriented Programming\\project.txt", userdelete);
-            } else if (access_type == 2) {
+            System.out.print("Enter your username and password.\n"
+                    + "Enter your login details.\n"
+                    + "* Username: ");
+            String username = cin.nextLine();
+            System.out.print("* Password: ");
+            String password = cin.nextLine();
 
+            if (s1.searchusername(username) && s1.searchpassword(password) == true) {
+                System.out.println("Press 1 to edit the restaurant's system.");
+                System.out.println("Press 2 to delete an account.");
+                System.out.println("press 3 to add new admin account");
+
+                int pick = cin.nextInt();
+
+                if (pick == 1) {
+                    test3.chooseOption();
+                } else if (pick == 2) {
+                    owner fileSearch = new owner();
+                    System.out.println("Enter the username to be deleted:");
+                    String userdelete = cin.nextLine();
+                    fileSearch.parseFile("D:\\study\\work\\oop\\project.txt", userdelete);
+                } else if (pick == 3) {
+                    System.out.print("Enter your username and password.\n"
+                            + "Enter your login details.\n"
+                            + "* Username: ");
+                    cin.nextLine();
+                    username = cin.nextLine();
+                    System.out.print("* Password: ");
+                    password = cin.nextLine();
+                    s1.writefile(username, password);
+                }
+
+            } else {
+                System.out.println("Error wrong username or password");
             }
         }
-    
-}
+
+        if (access_type == 2) {
+
+        } else if (access_type == 3) {
+
+        }
+    }
 }
