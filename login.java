b4bb7c1307970254c3951ebb829fbdb59c85226a
seif <74967136+seif1118@users.noreply.github.com>
2021-11-16 16:52:30 +0200

    Update login.java

--- login.java
+++ login.java
@@ -1,10 +1,11 @@
 package rms;
 
 import java.io.*;
+import java.util.*;
 
 public class login {
 
-    private static String username, password;
+    //private static String username, password;
 
     public login() {
 
@@ -12,30 +13,54 @@ public class login {
 
     public void createfile() {
         try {
-            File file = new File("C:\\Users\\PC\\Desktop\\project.txt");
+            File file = new File("D:\\study\\work\\oop\\project.txt");
             if (file.createNewFile()) {
                 System.out.println("New File is created!");
             } else {
                 System.out.println("File already exists.");
             }
         } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println("An error occurred." + e);
+            //e.printStackTrace();
         }
     }
 
-    public void writefile(String username,String password) {
+    public void writefile(String username, String password) {
         try {
-            try (FileWriter mywrite = new FileWriter("C:\\Users\\PC\\Desktop\\project.txt", true)) {
+            try (FileWriter mywrite = new FileWriter("D:\\study\\work\\oop\\project.txt", true)) {
                 mywrite.write(username);
                 mywrite.write(password);
             }
         } catch (IOException ex) {
-            System.out.println("An error occurred.");
-            ex.printStackTrace();
+            System.out.println("An error occurred." + ex);
+            //ex.printStackTrace();
         }
     }
 
+    public boolean searchusername(String username) throws FileNotFoundException {
+        File file = new File("D:\\study\\work\\oop\\project.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile.contains(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchpassword(String password) throws FileNotFoundException {
+        File file = new File("D:\\study\\work\\oop\\project.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile.contains(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+/*
     public void setusername(String username) {
         login.username = username;
     }
@@ -51,5 +76,5 @@ public class login {
     public String getpassword() {
         return password;
     }
-
+*/
 }
