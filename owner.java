25f6a7cc849796370f55dc5696c9f688b00ade44
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2021-11-16 22:07:44 +0200

    Update owner.java

--- owner.java
+++ owner.java
@@ -10,16 +10,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 
-public class owner{
-branches testing = new branches(); //Testing the methods
-menu testing2 = new menu();
+public class owner {
+
+    branches testing = new branches(); //Testing the methods
+    menu testing2 = new menu();
     Scanner scan = new Scanner(System.in);
 
     public void readfile() {
         try {
             String strcurrentline;
             //File myobj = new File("D:\\study\\work\\oop\\project.txt");
-            BufferedReader myobj = new BufferedReader(new FileReader("C:\\Users\\PC\\Desktop\\project.txt"));
+            BufferedReader myobj = new BufferedReader(new FileReader("D:\\MIU Courses\\Object Oriented Programming\\project.txt"));
             try (Scanner myreader = new Scanner(myobj)) {
                 while ((strcurrentline = myobj.readLine()) != null) {
 
@@ -48,7 +49,7 @@ menu testing2 = new menu();
     }
 
     public void deletefile(int choose) {
-        File myobj = new File("C:\\Users\\PC\\Desktop\\project.txt");
+        File myobj = new File("D:\\MIU Courses\\Object Oriented Programming\\project.txt");
         if (choose == 1) {//handle the error if the user enters letter not a number
             if (myobj.delete()) {
                 System.out.println(myobj.getName() + ":file is deleted successfully");
@@ -61,43 +62,69 @@ menu testing2 = new menu();
     }
 
     public String delete(String username, String password) {
-        File myobj = new File("C:\\Users\\PC\\Desktop\\project.txt");
+        File myobj = new File("D:\\MIU Courses\\Object Oriented Programming\\project.txt");
         username = "";
         password = "";
         return null;
     }
 
-    public void chooseOption() {
+    public void chooseOption() throws FileNotFoundException {
         System.out.println("Please pick what do you want to modify.\n"
                 + "1 - Branches\n"
                 + "2 - Menu\n"
                 + "3 - Employees\n");
         int x;
         x = scan.nextInt();
-        if(x==1){
-                System.out.println("1- Check the current branches.\n"
-                        +"2 - Add a new branch.\n"
-                        +"3 - Edit a branch.\n"
-                        +"4 - Remove a branch.\n");
-                int BranchChoice = scan.nextInt();
-                String y = scan.nextLine();
-                if(BranchChoice == 1){
-                testing.readBranches();
-                testing.readAddedBranches();
-                testing.showbranches();
-                } else if (BranchChoice == 2){
+        int MenuChoice;
+        if (x == 1) {
+            System.out.println("1- Check the current branches.\n"
+                    + "2 - Add a new branch.\n"
+                    + "3 - Edit a branch.\n"
+                    + "4 - Remove a branch.\n");
+            int BranchChoice = scan.nextInt();
+            String y = scan.nextLine();
+            if (BranchChoice == 1) {
+                testing.showAddedBranches();
+            } else if (BranchChoice == 2) {
                 testing.addBranch();
-                //testing.writeinitialBranches();
                 testing.writeAddedBranches();
                 testing.readBranches();
                 testing.readAddedBranches();
-                } else if (BranchChoice == 2){
-                
+            } else if (BranchChoice == 3) {
+                System.out.println("Please enter a branch to search for.");
+                String NameOfBranch = scan.nextLine();
+                testing.searchBranches(NameOfBranch);
+            }
+        } else if (x == 2) {
+            System.out.println("1- Check the current menu.\n"
+                    + "2 - Add a new menu item.\n"
+                    + "3 - Edit a menu item.\n"
+                    + "4 - Remove a menu item.\n");
+            MenuChoice = scan.nextInt();
+            if (MenuChoice == 1) {
+                //testing2.writeAddedMainItems();
+                testing2.defineMaps();
+                //testing2.writeAddedMainItems();
+                //testing2.showAddedMenuItems();
+            } else if (MenuChoice == 2) {
+                System.out.println("What course do you want to add a menu item to?");
+                int coursechoice;
+                System.out.println("1- Main course.\n"
+                        + "2 - Side items.\n"
+                        + "3 - Drink items.\n"
+                        + "4 - Dessert items.\n");
+                coursechoice = scan.nextInt();
+                if (coursechoice == 1) {                   
+                    testing2.defineMaps();
+                    testing2.AddMainToMenu();
                 }
-        } else if (x==2){
-        testing2.orderFood();
-        } else if(x==3){
-        
+                //testing2.AddItemToMenu();
+                //testing2.writeAddedMenuItems();
+                //testing2.readAddedMenuItems();
+                //testing2.showAddedMenuItems();
+            }
+        } else if (x == 3) {
+
         }
     }
 ;
