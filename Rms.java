2fddb9a4438713d6d7b88a3225e65cb15c10a310
Shady Amr <contact@shadyamr.me>
2021-11-16 22:50:57 +0200

    Added waiter login with ability to add and delete by admin.

--- Rms.java
+++ Rms.java
@@ -8,6 +8,7 @@ public class Rms
         Scanner cin = new Scanner(System.in);
         login s1 = new login();
         owner s2 = new owner();
+        waiter employee = new waiter();
         branches test = new branches();
         menu test2 = new menu();
         owner test3 = new owner();
@@ -29,18 +30,21 @@ public class Rms
             if (s1.searchusername(username) && s1.searchpassword(password) == true) 
             {
                 System.out.println("Press 1 to edit the restaurant's system.");
-                System.out.println("Press 2 to delete an account.");
-                System.out.println("press 3 to add new admin account");
+                System.out.println("Press 2 to delete an admin account.");
+                System.out.println("Press 3 to add new admin account");
+                System.out.println("Press 4 to add new waiter account.");
+                System.out.println("Press 5 to delete a waiter account.");
 
                 int pick = cin.nextInt();
-
+                
                 if (pick == 1) {
                     test3.chooseOption();
                 } else if (pick == 2) {
                     owner fileSearch = new owner();
                     System.out.println("Enter the username to be deleted:");
+                    cin.nextLine();
                     String userdelete = cin.nextLine();
-                    fileSearch.parseFile("D:\\study\\work\\oop\\project.txt", userdelete);
+                    fileSearch.parseFile("D:\\MIU Courses\\Object Oriented Programming\\project.txt", userdelete);
                 } else if (pick == 3) {
                     System.out.print("Enter your username and password.\n"
                             + "Enter your login details.\n"
@@ -50,6 +54,21 @@ public class Rms
                     System.out.print("* Password: ");
                     password = cin.nextLine();
                     s1.writefile(username, password);
+                } else if(pick == 4) {
+                   System.out.print("Enter your username and password.\n"
+                            + "Enter your login details.\n"
+                            + "* Username: ");
+                    cin.nextLine();
+                    username = cin.nextLine();
+                    System.out.print("* Password: ");
+                    password = cin.nextLine();
+                    employee.writefile(username, password);
+                } else if (pick == 5) {
+                    waiter fileSearch = new waiter();
+                    cin.nextLine();
+                    System.out.println("Enter the username to be deleted:");
+                    String userdelete = cin.nextLine();
+                    fileSearch.parseFile("D:\\MIU Courses\\Object Oriented Programming\\waiter.txt", userdelete);
                 }
 
             } 
@@ -64,7 +83,20 @@ public class Rms
             customer user = new customer();
             user.customerChoose();
         } else if (access_type == 3) {
-
+                        System.out.print("Enter your username and password.\n"
+                    + "Enter your login details.\n"
+                    + "* Username: ");
+            String username = cin.nextLine();
+            System.out.print("* Password: ");
+            String password = cin.nextLine();
+            if (employee.searchusername(username) && employee.searchpassword(password) == true) 
+            {
+                System.out.println("Login successful!");
+            }
+            else 
+            {
+                System.out.println("Error wrong username or password");
+            }
         }
     }
 }
--- waiter.java
+++ waiter.java
@@ -1,4 +1,74 @@
 package rms;
+
+import java.util.*;
+import java.io.*;
+import static java.lang.System.exit;
+
 public class waiter {
-    
+
+    public void writefile(String username, String password) {
+        try {
+            try (FileWriter mywrite = new FileWriter("D:\\MIU Courses\\Object Oriented Programming/waiter.txt", true)) {
+                mywrite.write(username);
+                mywrite.write(password);
+            }
+        } catch (IOException ex) {
+            System.out.println("An error occurred." + ex);
+            //ex.printStackTrace();
+        }
+    }
+
+    public boolean searchusername(String username) throws FileNotFoundException {
+        File file = new File("D:\\MIU Courses\\Object Oriented Programming/waiter.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile.contains(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchpassword(String password) throws FileNotFoundException {
+        File file = new File("D:\\MIU Courses\\Object Oriented Programming/waiter.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile.contains(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void parseFile(String fileName, String searchStr) throws FileNotFoundException { //Search
+        Scanner scan = new Scanner(new File(fileName));
+        while (scan.hasNext()) {
+            String line = scan.nextLine().toLowerCase();
+            if (line.contains(searchStr)) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    public void deletefile(int choose) {
+        File myobj = new File("D:\\MIU Courses\\Object Oriented Programming\\waiter.txt");
+        if (choose == 1) {//handle the error if the user enters letter not a number
+            if (myobj.delete()) {
+                System.out.println(myobj.getName() + ":file is deleted successfully");
+            } else {
+                System.out.println("unexpected error occured");
+            }
+        } else {
+            exit(0);
+        }
+    }
+
+    public String delete(String username, String password) {
+        File myobj = new File("D:\\MIU Courses\\Object Oriented Programming\\waiter.txt");
+        username = "";
+        password = "";
+        return null;
+    }
 }
