bdce2fa631b37f9c8e83d7f1b92be4722e7cd2c1
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2021-11-16 19:44:59 +0200

    Update branches.java

--- branches.java
+++ branches.java
@@ -1,6 +1,8 @@
 package rms;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,9 +16,7 @@ public class branches {
     protected int choosenBranchName;
     protected String choosenBranch;
     protected boolean check = true;
-    protected ArrayList<String> branchesNames = new ArrayList<>(Arrays.asList("El-Nozha", "El-Maady", "Madinet-Nasr"));
     protected ArrayList<String> AddedBranchesNames = new ArrayList<>();
-    protected double CombinedSizes = branchesNames.size() + AddedBranchesNames.size();
     public branches() {
         this.choosenBranchName = 0;
     }
@@ -28,13 +28,13 @@ public class branches {
 
     public void chooseBranch() {
         System.out.println("Please choose one of our branches on the screen.\n");
-        for (int i = 0; i < branchesNames.size(); i++) {
-            System.out.println(i + " for our " + branchesNames.get(i) + " branch.\n");
+        for (int i = 0; i < AddedBranchesNames.size(); i++) {
+            System.out.println(i + " for our " + AddedBranchesNames.get(i) + " branch.\n");
         }
         do {
             check = true;
             choosenBranchName = scan.nextInt();
-            if (choosenBranchName > branchesNames.size()) {
+            if (choosenBranchName > AddedBranchesNames.size()) {
                 check = false;
                 System.out.println("Invalid option, please enter a valid one!");
             }
@@ -45,28 +45,36 @@ public class branches {
         System.out.println("Enter the name of the new branch you want to add.\n");
         String x;
         x = scan.nextLine();
-        AddedBranchesNames.add(x);
+        AddedBranchesNames.add(x);       
     }
+    
+    public void showAddedBranches(){
+    try {
+            String strcurrentline;
+            BufferedReader myobj = new BufferedReader(new FileReader("D:\\MIU Courses\\Object Oriented Programming\\branches.txt"));
+            try (Scanner myreader = new Scanner(myobj)) {
+                while ((strcurrentline = myobj.readLine()) != null) {
 
-    public void showbranches() {
-        int y = 1,z = 1;
-        System.out.println("The current branches are: ");
-        for (int i = 0; i < CombinedSizes; i++) {
-            System.out.println(y + " - " + branchesNames.get(i) + "\n");
-            y++;
+                    System.out.println(strcurrentline);
+                }
+                int data = myobj.read();
+                while (data != -1) {
+                    data = myobj.read();
+                }
+                myobj.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("an error occured");
         }
-        /*for (int i = 0; i < AddedBranchesNames.size(); i++) {
-            System.out.println(y + " - " + AddedBranchesNames.get(i) + "\n");
-            y++;
-        }*/
     }
 
     public void writeinitialBranches() {
 try {
-            try (FileWriter mywrite = new FileWriter("C:\\Users\\PC\\Desktop\\branches.txt",true)) {
+            try (FileWriter mywrite = new FileWriter("D:\\MIU Courses\\Object Oriented Programming\\branches.txt",true)) {
                 {
-                    for (int i = 0; i < branchesNames.size(); i++) {
-                        mywrite.write(branchesNames.get(i) + "\n");
+                    for (int i = 0; i < AddedBranchesNames.size(); i++) {
+                        mywrite.write(AddedBranchesNames.get(i) + "\n");
                     }
                 }
                 mywrite.close();
@@ -79,10 +87,10 @@ try {
 
     public void readBranches() {
         try {
-            BufferedReader myobj = new BufferedReader(new FileReader("C:\\Users\\PC\\Desktop\\branches.txt"));
+            BufferedReader myobj = new BufferedReader(new FileReader("D:\\MIU Courses\\Object Oriented Programming\\branches.txt"));
             try (Scanner myreader = new Scanner(myobj)) {
                 while (myreader.hasNext()) {
-                    branchesNames.add(myreader.next());
+                    AddedBranchesNames.add(myreader.next());
                 }
                 myobj.close();
             }
@@ -94,7 +102,7 @@ try {
     
     public void writeAddedBranches() {
 try {
-            try (FileWriter mywrite = new FileWriter("C:\\Users\\PC\\Desktop\\branches.txt",true)) {
+            try (FileWriter mywrite = new FileWriter("D:\\MIU Courses\\Object Oriented Programming\\branches.txt",true)) {
                 {
                     for (int i = 0; i < AddedBranchesNames.size(); i++) {
                         mywrite.write(AddedBranchesNames.get(i) + "\n");
@@ -110,7 +118,7 @@ try {
     
     public void readAddedBranches() {
         try {
-            BufferedReader myobj = new BufferedReader(new FileReader("C:\\Users\\PC\\Desktop\\branches.txt"));
+            BufferedReader myobj = new BufferedReader(new FileReader("D:\\MIU Courses\\Object Oriented Programming\\branches.txt"));
             try (Scanner myreader = new Scanner(myobj)) {
                 while (myreader.hasNext()) {
                     AddedBranchesNames.add(myreader.next());
@@ -123,4 +131,21 @@ try {
         }
 }
     
+ public void searchBranches(String branchName) throws FileNotFoundException{
+ File file = new File("D:\\MIU Courses\\Object Oriented Programming/admin.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile.contains(branchName)) {
+                System.out.println("Branch name has been found, please enter the new name you want.");
+            }
+        } 
+}
+ 
+ public void removeBranch(String branchname){
+ int index;
+ index = scan.nextInt();
+ AddedBranchesNames.remove(index);
+ }
+ 
 }
