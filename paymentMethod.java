3657fab4b8d0bd7b2fce54c14ccf4c322bb738e3
Shady Amr <contact@shadyamr.me>
2021-11-16 19:01:24 +0200

    Updated paymentMethod with CVV Counter Check.

--- paymentMethod.java
+++ paymentMethod.java
@@ -5,8 +5,19 @@ public class paymentMethod
 {
     protected long card_number;
     protected int cvv;
+    protected boolean cvv_check = true;
     protected String exp_date;
     Scanner cin = new Scanner(System.in);
+    public int convertIntegerToDigit(int x)
+    {
+        int count = 0;
+        while(x != 0)
+        {
+            x /= 10;
+            ++count;
+        }
+        return count;
+    }
     public void paymentMethod() 
     {
         String paymentMsg = "*** What is your payment method? ***\n"
@@ -28,8 +39,17 @@ public class paymentMethod
                 cin.nextLine();
                 System.out.print("* Exp. Date (MM/YY): ");
                 exp_date = cin.nextLine();
-                System.out.print("* CVV: ");
-                cvv = cin.nextInt();
+                System.out.print("* CVV: ");                
+                do
+                {
+                    cvv_check = true;
+                    cvv = cin.nextInt();
+                    if(convertIntegerToDigit(cvv) > 3 || convertIntegerToDigit(cvv) < 3)
+                    {
+                        cvv_check = false;
+                        System.out.println("* [Error]: CVV contains 3 numbers. Please enter a valid CCV!");
+                    }
+                } while(!cvv_check);
                 
                 break;
             default:
