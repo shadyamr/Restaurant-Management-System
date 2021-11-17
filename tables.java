844bfbdb3dc9393ee9398cd71f1a9f4a5e8c67b9
Ahmed Hossam <86417843+XIK9@users.noreply.github.com>
2021-11-17 02:49:14 +0200

    Update tables.java

--- tables.java
+++ tables.java
@@ -3,12 +3,20 @@ package rms;
 import java.util.*;
 
 public class tables {
+
     Scanner scan = new Scanner(System.in);
-   protected double tablechoice;
-   public void reserveTable(){
-   System.out.println("Do you want a table in the smokers' area?\n"
-           + "1 - Yes\n"
-           + "2 -  No\n");
-   tablechoice = scan.nextInt();
-}
+    protected int tablechoice;
+    protected String tabletext;
+
+    public void reserveTable() {
+        System.out.println("Do you want a table in the smokers' area?\n"
+                + "1- Yes\n"
+                + "2- No\n");
+        tablechoice = scan.nextInt();
+        if (tablechoice == 1) {
+            tabletext = "Yes";
+        } else {
+            tabletext = "No";
+        }
+    }
 }
