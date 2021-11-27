9b430f5d13cb90dc06507cbcd6f449f72a8993a0
Shady Amr <contact@shadyamr.me>
2021-11-27 22:13:48 +0200

    Protected credit card numbers

--- paymentMethod.java
+++ paymentMethod.java
@@ -4,17 +4,15 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class paymentMethod {
-
-    protected String customerinfo1;
-    protected long customerinfo2;
-    protected long card_number;
+public class paymentMethod 
+{
+    protected String card_number, exp_date;
     protected int cvv;
     protected boolean cvv_check = true;
-    protected String exp_date;
     Scanner cin = new Scanner(System.in);
 
-    public int convertIntegerToDigit(int x) {
+    public int convertIntegerToDigit(int x) 
+    {
         int count = 0;
         while (x != 0) {
             x /= 10;
@@ -22,30 +20,47 @@ public class paymentMethod {
         }
         return count;
     }
-
-    public void paymentMethod() {
+    
+    public void protectCardNumb(String card)
+    {
+        String hiddenNumbers = "", last4 = card.substring(card.length()-4, card.length());
+        int stars = card.length() - 4;
+        for(int i = 0; i < stars; i++)
+        {
+            hiddenNumbers += "*";
+        }
+        System.out.println(hiddenNumbers + last4);
+    }
+    
+    public void paymentMethod() 
+    {
         String paymentMsg = "*** What is your payment method? ***\n"
                 + "* 1. Cash\n"
                 + "* 2. Credit or Debit Card.";
         System.out.println(paymentMsg);
         int default_payment = cin.nextInt();
-        switch (default_payment) {
+        switch (default_payment) 
+        {
             case 1:
                 String cashmsg = "A waiter will collect your payment!\n";
                 System.out.println(cashmsg);
                 break;
             case 2:
                 System.out.println("** Add Card");
-                System.out.print("* Card Number: ");
-                card_number = cin.nextLong();
                 cin.nextLine();
+                System.out.print("* Card Number: ");
+                //card_number = cin.nextLong();
+                card_number = cin.nextLine();
+                protectCardNumb(card_number);
                 System.out.print("* Exp. Date (MM/YY): ");
                 exp_date = cin.nextLine();
                 System.out.print("* CVV: ");
-                do {
+                do 
+                {
                     cvv_check = true;
                     cvv = cin.nextInt();
-                    if (convertIntegerToDigit(cvv) > 3 || convertIntegerToDigit(cvv) < 3) {
+                    if (convertIntegerToDigit(cvv) > 3 || convertIntegerToDigit(cvv) < 3) 
+                    {
                         cvv_check = false;
                         System.out.println("* [Error]: CVV contains 3 numbers. Please enter a valid CCV!");
                     }
